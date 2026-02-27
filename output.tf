1de1b18330dbefe94233ebc1d9e4a1d6098206bb
msoni00309 <msoni00309@gmail.com>
2026-02-27 22:13:58 +0530

    Refactor output variables in output.tf
    Refactor output variables to group public IPs, SSH commands, and HTTP URLs for Amazon Linux and Ubuntu Linux.

--- output.tf
+++ output.tf
@@ -1,12 +1,20 @@
-output "public_ip" {
-  value = aws_instance.web.public_ip
-  value = aws_instance.ubuntu_web.public_ip
+output "public_ips" {
+  value = {
+    amazon_linux = aws_instance.web.public_ip
+    ubuntu_linux = aws_instance.ubuntu_web.public_ip
+  }
 }
-output "ssh_example" {
-  value = "ssh -i /home/ec2-user/tf-ansible-nginx-web/myRSA_SSH_Key.pem ec2-user@${aws_instance.web.public_ip}"
-  value = "ssh -i /home/ec2-user/tf-ansible-nginx-web/myRSA_SSH_Key.pem ubuntur@${aws_instance.ubuntu_web.public_ip}"
+
+output "ssh_examples" {
+  value = {
+    amazon_linux = "ssh -i /home/ec2-user/tf-ansible-nginx-web/myRSA_SSH_Key.pem ec2-user@${aws_instance.web.public_ip}"
+    ubuntu_linux = "ssh -i /home/ec2-user/tf-ansible-nginx-web/myRSA_SSH_Key.pem ubuntu@${aws_instance.ubuntu_web.public_ip}"
+  }
 }
-output "http_url" {
-  value = "http://${aws_instance.web.public_ip}"
-  value = "http://${aws_instance.ubuntu_web.public_ip}"
+
+output "http_urls" {
+  value = {
+    amazon_linux = "http://${aws_instance.web.public_ip}"
+    ubuntu_linux = "http://${aws_instance.ubuntu_web.public_ip}"
+  }
 }
